57a39a5a06921870e5fe17673e50dd218e39bb5d
monicordovilla <monicordovilla98@gmail.com>
2021-06-05 14:09:38 +0200

    [Solo administrador puede iniciar sesión]

--- src/store/session.js
+++ src/store/session.js
@@ -1,5 +1,5 @@
 //https://github.com/juanwmedia/vuex-desde-cero
-import { auth } from '../firebase.js'
+import { auth, db } from '../firebase.js'
 
 const state = {
     user: null,
@@ -30,7 +30,22 @@ const actions = {
 
     async doLogin({ commit }, { email, password }){
         await auth.signInWithEmailAndPassword(email,password);
-        //si current user (id usuario) no en usuarios
+        //si current user (id usuario) no en personas ni facilitadores
+        const document = await db.doc(`users/${auth.currentUser}`).get();
+
+        if (document.exists){
+            if (document.data.Rol!="Facilitador" && document.data.Rol!="Persona"){
+                commit("setUser", auth.currentUser);
+            } else {
+                //si está lo echo con un logout
+                await auth.signOut();
+                commit("setUser", null);
+            }
+        } else {
+            commit("setUser", auth.currentUser);
+        }
+
+
         commit("setUser", auth.currentUser);
         //si está lo echo con un logout
     },
